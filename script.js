84d1e23f5b05255e8bca416ff0be5223d02e2228
Zachariah Gregg <gregg_z6986@hotmail.com>
2024-08-21 07:36:40 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const firstName = 'Zachariah';
+const lastName = 'Gregg';
+let fullName = `${firstName} ${lastName}.`;
+console.log(fullName); 
+
+let population = 2665000; 
+console.log(population);
+console.log(population * 3);
+
+let javaScriptIsCool = true;
+
+let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i <= 4; i++) {
+    console.log(i);
+}
